1b7d3dd8889b40bf538b4a11a6f03c6d78ced431
Christoph Bodynek <christophbodynek@christophs-imac.speedport_w_724v_typ_a_05011603_05_020>
2017-10-08 13:42:13 +0200

    first Commit

---
+++ window-shopper/window-shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Christoph Bodynek on 08.10.17.
+//  Copyright © 2017 Christoph Bodynek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
